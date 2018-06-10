f9eafcf6e299f35fd0e30637850733341551a885
Brent Walther <brentwalther@gmail.com>
2018-06-10 10:28:18 -0700

    Add some javadoc to GuiScreenUtil.

--- src/main/java/net/brentwalther/controllermod/ui/GuiScreenUtil.java
+++ src/main/java/net/brentwalther/controllermod/ui/GuiScreenUtil.java
@@ -8,6 +8,10 @@ public class GuiScreenUtil {
 
   private GuiScreenUtil() {}
 
+  /**
+   * Push an arbitrary {@link GuiScreen}. If the screen is a {@link ModScreen}, we will keep track
+   * of the current screen so that is can be replaced later with a call to{@link #popScreen()}.
+   */
   public static void pushScreen(GuiScreen screen) {
     Minecraft minecraft = Minecraft.getMinecraft();
     if (screen instanceof ModScreen) {
@@ -16,6 +20,10 @@ public class GuiScreenUtil {
     minecraft.displayGuiScreen(screen);
   }
 
+  /**
+   * Pops the current {@link ModScreen} (if one is present) and displays the screen that was being
+   * shown prior.
+   */
   public static void popScreen() {
     Minecraft minecraft = Minecraft.getMinecraft();
     if (!(minecraft.currentScreen instanceof ModScreen)) {
@@ -24,6 +32,9 @@ public class GuiScreenUtil {
     minecraft.displayGuiScreen(((ModScreen) minecraft.currentScreen).getParent());
   }
 
+  /**
+   * Refreshes the current screen. This will call {@link GuiScreen#initGui()} on the screen again.
+   */
   public static void refreshCurrentScreen() {
     Minecraft minecraft = Minecraft.getMinecraft();
     minecraft.displayGuiScreen(minecraft.currentScreen);
